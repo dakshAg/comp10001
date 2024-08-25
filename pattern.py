ecc8e2b089463f89f0ea61c219af858e550e3f85
Daksh Agrawal <daksh.agrawal@student.unimelb.edu.au>
2024-08-25 14:33:44 +1000

    w5

---
+++ pattern.py
@@ -0,0 +1,26 @@
+for i in range(10):
+    print(" "*(10-i) + "*"*(2*i))
+    
+for i in range(5):
+    print("*"*(5-i)*2)
+for i in range(3,-1,-1):
+    print("*"*(5-i)*2)
+
+for i in range(2):
+    print("*"*10)
+for i in range(2):
+    print("*"*2)
+for i in range(1):
+    print("*"*10)
+for i in range(2):
+    print(" "*8+"*"*2)
+for i in range(2):
+    print("*"*10)
+
+
+for i in range(4):
+    print("*"*2+" "*6+"*"*2)
+for i in range(1):
+    print("*"*10)
+for i in range(4):
+    print("*"*2+" "*6+"*"*2)
